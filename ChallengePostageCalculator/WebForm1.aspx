10e9521d152cc64f61e0698d2f1ed488d48cd794
Josh <josharmstrongphoto@gmail.com>
2018-01-27 14:18:47 -0800

    Initial commit

---
+++ ChallengePostageCalculator/WebForm1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ChallengePostageCalculator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Postal Calculator<br />
+        <br />
+        Width:&nbsp;
+        <asp:TextBox ID="widthTextBox" runat="server" OnTextChanged="handle" AutoPostBack="True"></asp:TextBox>
+        <br />
+        Height:&nbsp;
+        <asp:TextBox ID="heightTextBox" runat="server" OnTextChanged="handle" AutoPostBack="True"></asp:TextBox>
+        <br />
+        Length:&nbsp;
+        <asp:TextBox ID="lengthTextBox" runat="server" OnTextChanged="handle" AutoPostBack="True"></asp:TextBox>
+        <br />
+        <br />
+        <asp:RadioButton ID="groundRadio" runat="server" GroupName="shippingOption" OnCheckedChanged="handle" Text="Ground" AutoPostBack="True" />
+        <br />
+        <asp:RadioButton ID="airRadio" runat="server" GroupName="shippingOption" OnCheckedChanged="handle" Text="Air" AutoPostBack="True" />
+        <br />
+        <asp:RadioButton ID="nextDayRadio" runat="server" GroupName="shippingOption" OnCheckedChanged="handle" Text="Next Day" AutoPostBack="True" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePostageCalculator/WebForm1.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostageCalculator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            output();
+        }
+
+        protected void handle (object sender, EventArgs e)
+        {
+            output();
+        }
+
+        private void output()
+        {
+            if (!valuesExist()) return;
+
+            int volume = 0;
+            if (!inputVolume(out volume)) return;
+
+            double postageMultiplier = getPostageMultiplier();
+
+            double cost = volume * postageMultiplier;
+
+            resultLabel.Text = string.Format("Your parcel will cost {0:C} to ship.", cost);
+        }
+        private bool valuesExist()
+        {
+            if (!airRadio.Checked && !groundRadio.Checked && !nextDayRadio.Checked) return false;
+            if (widthTextBox.Text.Trim().Length == 0 || heightTextBox.Text.Trim().Length == 0) return false;
+            return true;
+        }
+        private bool inputVolume(out int volume)
+        {
+            volume = 0;
+            int width = 0;
+            int height = 0;
+            int length = 0;
+
+            if (!int.TryParse(widthTextBox.Text.Trim(), out width)) return false;
+            if (!int.TryParse(heightTextBox.Text.Trim(), out height)) return false;
+            if (!int.TryParse(lengthTextBox.Text.Trim(), out length)) length = 1;
+
+            volume = width * height * length;
+
+            return true;
+        }
+        private double getPostageMultiplier()
+        {
+            if (groundRadio.Checked) return .15;
+            else if (airRadio.Checked) return .25;
+            else if (nextDayRadio.Checked) return .45;
+            else return 0;
+        }
+    }
+}
